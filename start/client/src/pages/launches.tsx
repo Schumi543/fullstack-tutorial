23f70d9104fcb88121dc44bd6e00fd3e150ab456
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-02-28 17:53:57 +0900

    initialize client

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,26 @@
-import React, { Fragment }  from 'react';
+import React, { Fragment } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import gql from "graphql-tag";
 
-interface LaunchesProps extends RouteComponentProps {}
+interface LaunchesProps extends RouteComponentProps { }
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
 }
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 export default Launches;
 
