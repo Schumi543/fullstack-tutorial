474d31cf9652581f64cbd867dc6c9b96302be753
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-02-28 17:33:13 +0900

    remove unnecesarry config

--- start/server/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    client: {
-        service: {
-            name: 'apollo-server-tutorial',
-            url: 'http://localhost:4000/graphql',
-            includes: ['src/datasources/*.js'],
-        }
-    }
-};
